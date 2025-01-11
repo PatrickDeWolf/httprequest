0d667ed0aeec6f49dd19277ace6c68459e868c88
PatrickDeWolf <64219368+PatrickDeWolf@users.noreply.github.com>
2025-01-11 17:08:30 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Using the XMLHttpRequest Object</h2>
+
+
+
+  
+<div id="demo">
+<button type="button" onclick="extern()">Change Content</button>
+</div>
+
+<script>
+function extern() {
+  var xhttp = new XMLHttpRequest();
+  
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "xmlhttp_info.php?lang=", true);
+  xhttp.send();
+}
+</script>
+
+</body>
+</html>
